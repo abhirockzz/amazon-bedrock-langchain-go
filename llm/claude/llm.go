46f427e6ac22df99cefb24eab2563fa8f7914550
Abhishek <abhirockzz@gmail.com>
2023-10-23 23:00:46 +0530

    change

--- llm/claude/llm.go
+++ llm/claude/llm.go
@@ -88,6 +88,8 @@ func (o *LLM) Generate(ctx context.Context, prompts []string, options ...llms.Ca
 		log.Fatal(err)
 	}
 
+	log.Println("payload\n", string(payloadBytes))
+
 	output, err := o.brc.InvokeModel(context.Background(), &bedrockruntime.InvokeModelInput{
 		Body:        payloadBytes,
 		ModelId:     aws.String(claudeV2ModelID),
